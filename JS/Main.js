26437058bb890aaa67c2bf0145d71a92b8d464af
a7madddddd <142698638+a7madddddd@users.noreply.github.com>
2024-09-24 18:19:26 +0300

    task 23-9

--- JS/Main.js
+++ JS/Main.js
@@ -1,4 +1,4 @@
-/// ques :1
+/// 23 - 9 Task 
 var num = 4;
 if (num % 2 === 0) {
     console.log("".concat(num, " is an event number."));
@@ -21,10 +21,10 @@ var admins = [
     { name: "mohammad", age: 28, isAdmin: false }
 ];
 var oldestAdmin = null;
-for (var i = 0; i < admins.length; i++) {
-    if (admins[i].isAdmin) {
-        if (oldestAdmin === null || admins[i].age > oldestAdmin.age) {
-            oldestAdmin = admins[i];
+for (var i_1 = 0; i_1 < admins.length; i_1++) {
+    if (admins[i_1].isAdmin) {
+        if (oldestAdmin === null || admins[i_1].age > oldestAdmin.age) {
+            oldestAdmin = admins[i_1];
         }
     }
 }
@@ -33,4 +33,45 @@ if (oldestAdmin) {
 }
 else {
     console.log("No admin found");
+}
+///// 24 - 9 task
+var nums = [1, 2, 3, 4, 5];
+var sum = nums[0] + nums[1] + nums[2] + nums[3] + nums[4];
+console.log(sum);
+var sum2 = [-8, 6, -7, 4];
+if (sum2[0] && sum2[1] && sum2[2] && sum2[3] > 0) {
+    console.log("not all numbers positive");
+}
+else {
+    console.log("all numbers positive");
+}
+var names = ["Alice", "Bob", "Christopher", "David"];
+var longName = "";
+for (var i = 0; i < names.length; i++) {
+    if (names[i].length > longName.length) {
+        longName = names[i];
+        console.log(longName);
+    }
+}
+var word = "wellcome";
+var char = 0;
+var varchar = "l";
+for (var i_2 = 0; i_2 < word.length; i_2++) {
+    if (word[i_2] == varchar) {
+        char++;
+    }
+}
+console.log(char);
+var primeArray = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (var i_3 = 0; i_3 < primeArray.length; i_3++) {
+    var isPrime = true;
+    for (var j = 2; j * j <= primeArray[i_3]; j++) {
+        if (primeArray[i_3] % j === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+    if (isPrime && primeArray[i_3] > 1) {
+        console.log(primeArray[i_3]);
+    }
 }
--- JS/Main.ts
+++ JS/Main.ts
@@ -1,49 +1,112 @@
-/// ques :1
-
+/// 23 - 9 Task 
 var num = 4;
 if (num % 2 === 0) {
-    console.log(`${num} is an event number.`);
+  console.log(`${num} is an event number.`);
 }
 else {
-    console.log(`${num} is odd number.`);
+  console.log(`${num} is odd number.`);
 }
 
 
 let price: number[] = [10, 30, 20];
 
-for (var n = 0; n < price.length ; n++) {
+for (var n = 0; n < price.length; n++) {
 
-    if(price[n] > 20) {
-        console.log(` ${n} is above 20.`);
-    }
-    else {
-        console.log(`${n} is not above 30.`);
-    }
+  if (price[n] > 20) {
+    console.log(` ${n} is above 20.`);
+  }
+  else {
+    console.log(`${n} is not above 30.`);
+  }
 }
 
 type Admin = {
-    name: string;
-    age: number;
-    isAdmin: boolean;
-  };
-let admins: Admin [] = [
-    {name :"ahmad", age:24, isAdmin: false},
-    {name :"ali", age:30, isAdmin: true},
-    {name :"mohammad", age:28, isAdmin: false}
+  name: string;
+  age: number;
+  isAdmin: boolean;
+};
+let admins: Admin[] = [
+  { name: "ahmad", age: 24, isAdmin: false },
+  { name: "ali", age: 30, isAdmin: true },
+  { name: "mohammad", age: 28, isAdmin: false }
 ];
 let oldestAdmin: Admin | null = null;
-  
-  for (let i = 0; i < admins.length; i++) {
-    if (admins[i].isAdmin) {
-      if (oldestAdmin === null || admins[i].age > oldestAdmin.age) {
-        oldestAdmin = admins[i];
-      }
+
+for (let i = 0; i < admins.length; i++) {
+  if (admins[i].isAdmin) {
+    if (oldestAdmin === null || admins[i].age > oldestAdmin.age) {
+      oldestAdmin = admins[i];
     }
   }
-  
-  if (oldestAdmin) {
-    console.log(`Oldest Admin: ${oldestAdmin.name}`);
-  } else {
-    console.log("No admin found");
+}
+
+if (oldestAdmin) {
+  console.log(`Oldest Admin: ${oldestAdmin.name}`);
+} else {
+  console.log("No admin found");
+}
+
+///// 24 - 9 task
+
+let nums: number[] = [1, 2, 3, 4, 5]
+
+let sum = nums[0]+nums[1]+nums[2]+nums[3]+nums[4]
+console.log(sum)
+
+
+
+let sum2 : number[] = [-8, 6, -7, 4]
+
+
+  if(sum2[0] && sum2[1] && sum2[2] && sum2[3] > 0){
+
+    console.log("not all numbers positive")
   }
-  
+  else{
+
+    console.log("all numbers positive")
+  }
+
+
+
+let names: string[] = ["Alice", "Bob", "Christopher", "David"]
+let longName = "";
+for(var i = 0; i <names.length; i++){
+
+  if (names[i].length > longName.length){
+     longName = names[i];
+
+    console.log(longName)
+  }
+}
+
+
+
+let word = "wellcome";
+let char = 0;
+let varchar = "l";
+
+for (let i = 0; i < word.length; i++) {
+  if (word[i] == varchar) {
+    char++;  
+  }
+}
+
+console.log(char);  
+
+let primeArray: number[] = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < primeArray.length; i++) {
+  let isPrime = true;
+
+  for (let j = 2; j * j <= primeArray[i]; j++) {
+    if (primeArray[i] % j === 0) {
+      isPrime = false;
+      break;  
+    }
+  }
+
+  if (isPrime && primeArray[i] > 1) {
+    console.log(primeArray[i]);
+  }
+}
